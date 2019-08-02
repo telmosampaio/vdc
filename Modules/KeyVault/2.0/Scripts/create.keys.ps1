fb07708133283e8522cbad629b3063c217d3a25b
Jorge Cotillo <jorge.cotillo@gmail.com>
2019-08-02 09:45:09 -0700

    added encryption task

---
+++ Modules/KeyVault/2.0/Scripts/create.keys.ps1
@@ -0,0 +1,23 @@
+[CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $VaultName,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $KeyName,
+        [Parameter(Mandatory=$false)]
+        [string]
+        $Destination
+    )
+if ($null -eq $Destination) {
+    $Destination = "HSM";
+}
+$result = Add-AzKeyVaultKey `
+    -VaultName $VaultName `
+    -Name $KeyName `
+    -Destination $Destination;
+
+return @{
+    KekUrl = $result.Id
+}
