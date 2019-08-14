22febb01457fd8c5793a48731d13a687d445ec88
kungumaraj nachimuthu <senthilkungumaraj@gmail.com>
2019-08-14 09:55:24 -0700

    Point-to-Site VPN added to On-Premise Archetype

---
+++ Modules/VirtualNetworkGateway/2.0/Scripts/ClientCert.sh
@@ -0,0 +1,6 @@
+PASSWORD=$1
+# Use a default value of "client" for USERNAME
+USERNAME=$2
+ipsec pki --gen --outform pem > ~/pki/"${USERNAME}Key.pem"    
+ipsec pki --pub --in ~/pki/"${USERNAME}Key.pem" | ipsec pki --issue --cacert ~/pki/caCert.pem --cakey ~/pki/caKey.pem --dn "CN=${USERNAME}" --san "${USERNAME}" --flag clientAuth --outform pem > ~/pki/"${USERNAME}Cert.pem"
+openssl pkcs12 -in ~/pki/"${USERNAME}Cert.pem" -inkey ~/pki/"${USERNAME}Key.pem" -certfile ~/pki/caCert.pem -export -out ~/pki/"${USERNAME}.p12" -password "pass:${PASSWORD}"
